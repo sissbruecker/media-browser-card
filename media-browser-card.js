905cbea055c1f98e5a39c77768cca923e25d08b3
Sascha Ißbrücker <sascha.issbruecker@googlemail.com>
2021-11-07 13:44:33 +0100

    Add media player selection

--- media-browser-card.js
+++ media-browser-card.js
@@ -13,6 +13,25 @@ const fileIcon = html`
   </svg>
 `;
 
+const BROWSER_PLAYER = {
+  entity_id: "browser",
+  attributes: {
+    friendly_name: "Browser"
+  }
+};
+
+function getMediaPlayers(hass) {
+  const filteredPlayers = Object.values(hass.states).filter(entity =>
+    entity.entity_id.match(/^media_player\./)
+    && (entity.attributes.supported_features & 131072) !== 0
+  );
+
+  return [
+    BROWSER_PLAYER,
+    ...filteredPlayers
+  ];
+}
+
 async function browseMedia(hass, mediaContentId) {
   return hass.callWS({
     type: "media_source/browse_media",
@@ -20,12 +39,23 @@ async function browseMedia(hass, mediaContentId) {
   });
 }
 
-async function playMedia(hass, config, item) {
+async function playMedia(hass, entity_id, item) {
   return hass.callService("media_player", "play_media", {
-    entity_id: config.player,
+    entity_id,
     media_content_id: item.media_content_id,
     media_content_type: item.media_content_type
   });
+}
+
+async function playMediaInBrowser(hass, item) {
+  const resolvedUrl = await hass.callWS({
+    type: "media_source/resolve_media",
+    media_content_id: item.media_content_id
+  });
+
+  // Ideally we would like to open a dialog, as is done here: https://github.com/home-assistant/frontend/blob/c26a59d8059497104ed52ad44b17146547f0173c/src/panels/media-browser/ha-panel-media-browser.ts#L96
+  // For now just open a new tab
+  window.open(resolvedUrl.url, "_blank");
 }
 
 function isDirectory(item) {
@@ -63,7 +93,9 @@ class MediaBrowserCard extends LitElement {
       config: {},
       _currentDirectoryItem: { state: true },
       _currentPlayingItemId: { state: true },
-      _currentPath: { state: true }
+      _currentPath: { state: true },
+      _availablePlayers: { state: true },
+      _selectedPlayer: { state: true }
     };
   }
 
@@ -86,6 +118,11 @@ class MediaBrowserCard extends LitElement {
       .card-header {
         display: flex;
         justify-content: space-between;
+        align-items: baseline;
+      }
+      
+      .card-header select {
+        width: auto;
       }
       
       .card-body {
@@ -129,6 +166,7 @@ class MediaBrowserCard extends LitElement {
   constructor() {
     super();
     this._currentPath = clientData.currentPath;
+    this._availablePlayers = [];
   }
 
   firstUpdated() {
@@ -139,8 +177,22 @@ class MediaBrowserCard extends LitElement {
     super.updated(changedProps);
 
     if (changedProps.has("hass")) {
+      // Update available media players
+      this._availablePlayers = this.hass ? getMediaPlayers(this.hass) : [];
+      const isSelectedPlayerAvailable = this._selectedPlayer
+        ? this._availablePlayers.some(player => player.entity_id === this._selectedPlayer.entity_id)
+        : false;
+      if (!isSelectedPlayerAvailable) {
+        const newPlayer = this._availablePlayers.find(player => player.entity_id === clientData.selectedPlayerId)
+          || this._availablePlayers[0];
+        this.selectPlayer(newPlayer);
+      }
+
+      // Update currently played items
       const playerState = this.hass ? this.hass.states[this.config.player] : null;
-      this._currentPlayingItemId = playerState ? decodeURI(playerState.attributes.media_content_id) : null;
+      this._currentPlayingItemId = playerState
+        ? decodeURI(playerState.attributes.media_content_id)
+        : null;
     }
   }
 
@@ -173,13 +225,25 @@ class MediaBrowserCard extends LitElement {
     this.loadCurrentDirectory();
   }
 
+  selectPlayer(player) {
+    this._selectedPlayer = player;
+    updateClientData({ selectedPlayerId: player ? player.entity_id : undefined });
+  }
+
   async loadCurrentDirectory() {
     const directoryId = this._currentPath[this._currentPath.length - 1] || undefined;
     this._currentDirectoryItem = await browseMedia(this.hass, directoryId);
   }
 
   async playItem(item) {
-    await playMedia(this.hass, this.config, item);
+    if (!this._selectedPlayer) return;
+
+    if (this._selectedPlayer.entity_id === BROWSER_PLAYER.entity_id) {
+      await playMediaInBrowser(this.hass, item);
+    } else {
+      await playMedia(this.hass, this._selectedPlayer.entity_id, item);
+    }
+
     if (!clientData.playedItemIds.includes(item.media_content_id)) {
       updateClientData({
         playedItemIds: [...clientData.playedItemIds, item.media_content_id]
@@ -195,17 +259,45 @@ class MediaBrowserCard extends LitElement {
       <link rel="stylesheet" href="https://unpkg.com/spectre.css/dist/spectre.min.css">
       <div class="card">
         <div class="card-header">
-          <h5 class="card-title">Media Browser</h5>
-          ${
-            this._currentPath.length > 0 ? html`
-              <button class="btn btn-sm" @click="${this.back}">Back</button>
-            ` : null
-          }
+          <div>
+            <h5>Media Browser</h5>
+            ${
+              this._currentPath.length > 0 ? html`
+                <button class="btn btn-sm" @click="${this.back}">Back</button>
+              ` : null
+            }
+          </div>
+          <div>
+            ${this.renderPlayerSelect()}
+          </div>
         </div>
         <div class="card-body">
           ${hasChildren ? this.renderFileList(children) : this.renderEmptyHint()}
         </div>
       </div>
+    `;
+  }
+
+  renderPlayerSelect() {
+    if (!this._availablePlayers.length) return null;
+
+    const selectedPlayerId = this._selectedPlayer
+      ? this._selectedPlayer.entity_id
+      : null;
+    const options = this._availablePlayers.map(player => {
+      const displayName = player.attributes.friendly_name || player.entity_id;
+      return html`
+        <option value="${player.entity_id}"
+                ?selected="${player.entity_id === selectedPlayerId}">${displayName}
+        </option>
+      `;
+    });
+
+    return html`
+      <select class="form-select select-sm"
+              @change="${(event) => this.selectPlayer(this._availablePlayers.find(player => player.entity_id === event.target.value))}">
+        ${options}
+      </select>
     `;
   }
 
